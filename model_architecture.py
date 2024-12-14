bc5280d5fc646bcfde5971d9bf174d799d9ebdc0
chrryyy <cherylbai7@gmail.com>
2024-12-13 19:29:00 -0500

    Removed BetaVAE

--- model_architecture.py
+++ model_architecture.py
@@ -102,69 +102,6 @@ class BetaScheduler(keras.callbacks.Callback):
         self.beta_var.assign(new_beta)
 
 
-class BetaVAE(keras.Model):
-    def __init__(self, encoder, decoder, beta=1.0, **kwargs):
-        super().__init__(**kwargs)
-        self.encoder = encoder
-        self.decoder = decoder
-        self.beta = beta
-        self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
-        self.reconstruction_loss_tracker = keras.metrics.Mean(name="reconstruction_loss")
-        self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
-
-    def call(self, inputs):
-        z_mean, z_log_var, z = self.encoder(inputs)
-        reconstruction = self.decoder(z)
-        return reconstruction
-
-    def get_config(self):
-        # Save encoder and decoder configuration, along with beta value
-        config = super().get_config()
-        config.update({
-            "encoder_config": self.encoder.get_config(),
-            "decoder_config": self.decoder.get_config(),
-            "beta": self.beta,
-        })
-        return config
-
-    @classmethod
-    def from_config(cls, config):
-        encoder = Model.from_config(config.pop("encoder_config"))
-        decoder = Model.from_config(config.pop("decoder_config"))
-        return cls(encoder=encoder, decoder=decoder, **config)
-
-    @property
-    def metrics(self):
-        return [
-            self.total_loss_tracker,
-            self.reconstruction_loss_tracker,
-            self.kl_loss_tracker,
-        ]
-
-    def train_step(self, data):
-        with tf.GradientTape() as tape:
-            z_mean, z_log_var, z = self.encoder(data)
-            reconstruction = self.decoder(z)
-            reconstruction_loss = tf.reduce_mean(
-                tf.reduce_sum(
-                    keras.losses.binary_crossentropy(data, reconstruction), axis=(1, 2)
-                )
-            )
-            kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
-            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
-            total_loss = reconstruction_loss + self.beta * kl_loss
-        grads = tape.gradient(total_loss, self.trainable_weights)
-        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
-        self.total_loss_tracker.update_state(total_loss)
-        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
-        self.kl_loss_tracker.update_state(kl_loss)
-        return {
-            "loss": self.total_loss_tracker.result(),
-            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
-            "kl_loss": self.kl_loss_tracker.result(),
-        }
-
-
 class TC_VAE(keras.Model):
     def __init__(self, encoder, decoder, beta=1.0, beta_tc=10.0, **kwargs):
         super().__init__(**kwargs)
